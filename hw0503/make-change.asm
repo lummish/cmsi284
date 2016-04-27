c0ce1426c0e379285142545b76e302e7dcdeea29
lummish <lummish@gmail.com>
2016-04-27 12:26:04 -0700

    instead trying to push rsi to stack

--- hw0503/make-change.asm
+++ hw0503/make-change.asm
@@ -7,9 +7,9 @@ main:
 		push 	rbx				; necessary convention
 		mov		rdi, [rsi + 8]	; move user entered amount to make change of into rdi
 		call	atoi			; turn contents of rdi into integer in rax 
-		;push	rax 			; so we can actually use this value in a second
 		mov 	rdi, quarter_format		; move rax back into rdi, rdi stores total remaining value
 		mov 	rsi, rax
+		push	rsi 			; so we can actually use this value in a second
 		call 	printf
 		
 add_quarters:

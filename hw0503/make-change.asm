1276449c04af59faa67fa22a45d474aa03b0a647
lummish <lummish@gmail.com>
2016-04-27 11:37:29 -0700

    moving pop and ret down to test origin of segfault 2

--- hw0503/make-change.asm
+++ hw0503/make-change.asm
@@ -12,13 +12,14 @@ main:
 		
 add_quarters:
 		mov 	rdx, 0 			; to track remainder
-		mov 	rcx, q		; set value of rcx to current coin value
+		pop 	rbx 			;necessary convention
+		ret
+		mov 	rcx, q			; set value of rcx to current coin value
 		div 	rcx 			; unsigned divide
 		push	rax				; push quotient to stack
 		push	rdx 			; want to store remaining value after removing coin
 		xor 	rax, rax 		; reset rax for next label
-		pop 	rbx 			;necessary convention
-		ret
+
 add_dimes:
 		pop		rax 			; pops previous remainder from top of stack and stores in rax
 		mov 	rdx, 0 			; to track remainder

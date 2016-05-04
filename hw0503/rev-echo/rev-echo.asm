eb72f710d3172938ad19f1055203bfe0fc988ed9
lummish <lummish@gmail.com>
2016-05-03 23:06:34 -0700

    cleaned up code

--- hw0503/rev-echo/rev-echo.asm
+++ hw0503/rev-echo/rev-echo.asm
@@ -11,19 +11,10 @@ main:
 		mov		r12, rsi 	; r12 stores pointer to rsi
 next:	
 		mov 	rdi, [r12] 	; move string to current arg to rdi
-		;push	r12			; store r12 for later use
 		call	strlen		; get length of arg and store in rax
-		mov		rdx, rax	; move contents of rax to rdx (last argument of rev range)
-		
-		;mov 	rsi, rdx
-		;mov 	rdi, num_format
-		;call 	printf
-		;call	puts
-		
-		;call 	printf
-		;mov		rdx, rax	
-		xor		rsi, rsi		; second arg (start of string) should be 0
-		push 	rdi				; to make sure rdi address remains constant
+		mov		rdx, rax	; move contents of rax to rdx (last argument of rev range)	
+		xor		rsi, rsi	; second arg (start of string) should be 0
+		push 	rdi			; to make sure rdi address remains constant
 		call	reverse_range_in_place
 		pop 	rsi
 		mov 	rdi, format

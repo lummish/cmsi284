85b1f581496c0bfc876bcb34236090a35e3d92f5
lummish <lummish@gmail.com>
2016-05-03 22:57:30 -0700

    cleaned up code and error handling

--- hw0503/madlibs/madlib-by-numbers.asm
+++ hw0503/madlibs/madlib-by-numbers.asm
@@ -9,7 +9,7 @@ main:
 		push 	rbx
 		sub		rdi, 2
 		cmp 	rdi, 0	; need to ensure that there are at least 0 words to substitute in 
-		;jl		error
+		jl		error
 		push 	rdi				; need to keep track of word count
 		mov 	rdi, [rsi + 8]	; store template in rdi
 		add		rsi, 16			
@@ -17,19 +17,22 @@ main:
 		pop		rsi						
 		call	madlib_by_numbers
 		mov		rsi, rdi
-		mov		rdi, debug_num
+		mov		rdi, output
 		mov 	rsi, rax
 		xor 	rax, rax
 		sub 	rsp, 8
 		call	printf
 		add		rsp, 8
+		jmp		end
+error:
+		mov 	rdi, error_format
+		call 	puts
+
+end:
 		pop 	rbx
 		ret
-;error:
-;		call puts
 error_format:
-		db "Oh noes",10,0
-debug_num:
+		db "Argument error.",10,0
+output:
 		db "%s",10,0
-debug_str:
-		db "Template: %s; Wordcount: %d",10,0
+

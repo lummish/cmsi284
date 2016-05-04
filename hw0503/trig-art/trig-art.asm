cc2f27176cd91bb447c94ef97c5b0201957c4014
lummish <lummish@gmail.com>
2016-05-03 22:48:38 -0700

    cleaned up unnecessary code from trigart

--- hw0503/trig-art/trig-art.asm
+++ hw0503/trig-art/trig-art.asm
@@ -4,8 +4,7 @@
 		extern printf
 		extern puts
 		section .text
-;output: db "%10f %10f %10f", 10, 0 
-;output: db "                                                                                ",0
+		
 output: db "--------------------------------------------------------------------------------",0
 plot_char: db "x"
 pi: dq 3.141592653589793
@@ -23,38 +22,23 @@ L0:
 		addsd	 xmm0, [center]	; offset central value
 		cvtsd2si rax, xmm0		; will bring these back once correctness is checkd
 		mov 	 [index], rax	; store rax as index value
-
-		;movsd 	 [index], xmm0
-		
-		;; Display the results.
-		;mov 	 rdi, output
-		;movsd 	 xmm0, [radian]
-		;movsd 	 xmm1, [sine]
-		;movsd 	 xmm2, [index]
-		;mov 	 rax, 3 ; 3 vector registers!
-		;call 	 printf
-		;mov 	rdi, output
-		;push	rdi
-		;mov 	rdi, rdx
-		;mov 	[text], output
-		mov 	rdi, text
-		mov 	rcx, 0		;counter
-char_replace:		
-		cmp		rax, rcx
-		je		next
-		inc		rcx
-		jmp 	char_replace
+		mov 	 rdi, text
+		mov 	 rcx, 0			;counter
+char_index:		
+		cmp		 rax, rcx
+		je		 next
+		inc		 rcx
+		jmp 	 char_index
 next:	
-		mov 	 byte [rdi + rcx], 'x'
-		push	 rdi
-		push	 rcx
+		mov 	 byte [rdi + rcx], 'x'	;change char at desired index to x
+		push	 rdi 					;to prevent changes to register by extern
+		push	 rcx					
 		xor      rax, rax
 		call	 printf
 		pop 	 rcx
 		pop		 rdi
-		mov 	 byte [rdi + rcx], ' '
+		mov 	 byte [rdi + rcx], ' '	;revert print string to all spaces
 
-		;mov 	 [text], rdi
 		movsd 	 xmm0, [radian]
 		movsd 	 xmm1, [pi]
 		divsd 	 xmm1, [delta]
@@ -64,19 +48,11 @@ next:
 		mulsd 	 xmm2, [two]
 		addsd 	 xmm1, xmm2 ; 2*pi + pi/delta
 		comisd 	 xmm0, xmm1
-		jc L0 ; CF is set if less than.
-		pop rbp
+		jc 		 L0 ; CF is set if less than.
+		pop 	 rbp
 		ret
 num_format:
 		db "%d",10,0
-
-;make_output:
-		;mov 	rax, [index]	; set counter to index value
-		;mov 	rdi, [output]
-		;mov 	[rdi + [index]], [plot_char]
-
-
-
 
  		section .data
 radian: dq 0.0

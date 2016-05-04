a98d47a9b8a4690675cdf9d3919cdb2b995d955e
lummish <lummish@gmail.com>
2016-05-03 23:06:55 -0700

    cleaned up code again

--- hw0503/sys_rename.asm
+++ hw0503/sys_rename.asm
@@ -5,15 +5,15 @@
 		extern		puts
 main:
 		push 	rbx
-		cmp		rdi, 3
+		cmp		rdi, 3			;ensure proper number of arugments
 		jne		error
-		mov		rdi, [rsi + 8]
-		mov		rsi, [rsi + 16]
-		mov		rax, 82
+		mov		rdi, [rsi + 8]	;get original name of file (should be first arg)
+		mov		rsi, [rsi + 16] ;get desired name of file
+		mov		rax, 82			;number of system call
 		syscall
-		cmp		rax, 0
+		cmp		rax, 0			;if exits with code other than 0, error.
 		jne		error
-		mov		rdx, rsi
+		mov		rdx, rsi		
 		mov		rsi, rdi
 		mov		rdi, format_str
 		call	printf
